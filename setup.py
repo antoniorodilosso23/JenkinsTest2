7149c994762faa15081ca99a590b0d576f3055d7
antrod <rodilossoa@gmail.com>
2024-03-11 20:28:58 +0100

    Build (setup.py)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JenkinsTest2',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Antonio Rodilosso',
+    author_email='antonio.rodilosso@iet.it',
+    description='Breve descrizione del tuo progetto',
+    url='https://github.com/antoniorodilosso23/JenkinsTest2',
+    install_requires=[
+        # Dipendenze del tuo progetto
+    ],
+)
